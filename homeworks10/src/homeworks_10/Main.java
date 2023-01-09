b4572a32d1fcdb5c657477cccd21e1741f1f5fbd
User <User@DESKTOP-UTCRT53>
2023-01-09 17:37:00 +0100

    ilkcommit

---
+++ homeworks10/src/homeworks_10/Main.java
@@ -0,0 +1,87 @@
+package homeworks_10;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner (System.in);
+		
+		System.out.println("lutfen bir plaka giriniz :");
+		
+		String girilenplaka =scan.nextLine();
+		//String girilenplaka1 =scan.nextLine();
+		//String girilenplaka2 =scan.nextLine();
+		//String girilenplaka3 =scan.nextLine();
+		//String girilenplaka4 =scan.nextLine();
+		List<String> list1=new ArrayList<String>();
+		
+		list1.add(girilenplaka);
+		//list1.add(girilenplaka1);
+		//list1.add(girilenplaka2);
+		//list1.add(girilenplaka3);
+		//list1.add(girilenplaka4);
+		
+		
+		
+		
+		PlakaTanima test = new PlakaTanima();
+		
+		test.setArac1plaka("26KV860"); 
+		test.setArac2plaka("31TT058");
+		test.setArac3plaka("26VS058");
+		test.setArac4plaka("26AU765");
+		test.setArac5plaka("26HL532");
+		
+		List<String> list=new ArrayList<String>();
+		
+		list.add(test.getArac1plaka());
+		list.add(test.getArac2plaka());
+		list.add(test.getArac3plaka());
+		list.add(test.getArac4plaka());
+		list.add(test.getArac5plaka());
+		
+	
+		//System.out.println(list);
+		//boolean areequal =true;
+		int sayac=0;
+		
+		for(int i=0;i<list.size();i++) {
+			
+			if(list1.get(0).equals(list.get(i))) {
+			
+				System.out.println(" girdiginiz arac calintidir " );
+			
+		sayac++;
+			
+		}
+		}		
+			
+			
+		
+		
+		
+		
+		
+		
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		 
+		
+		
+		
+		
+		
+	}
+
+	}
---
+++ homeworks10/src/homeworks_10/PlakaTanima.java
@@ -0,0 +1,187 @@
+package homeworks_10;
+
+public class PlakaTanima {
+	
+	private String arac1plaka ;
+	private String arac1isim  ;
+	private String arac1marka ;
+	private String arac1adres ;
+	private String arac1durum ;
+	
+	private String arac2plaka ;
+	private String arac2isim  ;
+	private String arac2marka ;
+	private String arac2adres ;
+	private String arac2durum ;
+	
+	private String arac3plaka ;
+	private String arac3isim  ;
+	private String arac3marka ;
+	private String arac3adres ;
+	private String arac3durum ;
+	
+	private String arac4plaka ;
+	private String arac4isim  ;
+	private String arac4marka ;
+	private String arac4adres ;
+	private String arac4durum ;
+	
+	private String arac5plaka ;
+	private String arac5isim  ;
+	private String arac5marka ;
+	private String arac5adres ;
+	private String arac5durum ;
+	
+	public String getArac1plaka() {
+		return arac1plaka;
+	}
+	public void setArac1plaka(String arac1plaka) {
+		this.arac1plaka = arac1plaka;
+	}
+	public String getArac1isim() {
+		return arac1isim;
+	}
+	public void setArac1isim(String arac1isim) {
+		this.arac1isim = arac1isim;
+	}
+	public String getArac1marka() {
+		return arac1marka;
+	}
+	public void setArac1marka(String arac1marka) {
+		this.arac1marka = arac1marka;
+	}
+	public String getArac1adres() {
+		return arac1adres;
+	}
+	public void setArac1adres(String arac1adres) {
+		this.arac1adres = arac1adres;
+	}
+	public String getArac1durum() {
+		return arac1durum;
+	}
+	public void setArac1durum(String arac1durum) {
+		this.arac1durum = arac1durum;
+	}
+	public String getArac2plaka() {
+		return arac2plaka;
+	}
+	public void setArac2plaka(String arac2plaka) {
+		this.arac2plaka = arac2plaka;
+	}
+	public String getArac2isim() {
+		return arac2isim;
+	}
+	public void setArac2isim(String arac2isim) {
+		this.arac2isim = arac2isim;
+	}
+	public String getArac2marka() {
+		return arac2marka;
+	}
+	public void setArac2marka(String arac2marka) {
+		this.arac2marka = arac2marka;
+	}
+	public String getArac2adres() {
+		return arac2adres;
+	}
+	public void setArac2adres(String arac2adres) {
+		this.arac2adres = arac2adres;
+	}
+	public String getArac2durum() {
+		return arac2durum;
+	}
+	public void setArac2durum(String arac2durum) {
+		this.arac2durum = arac2durum;
+	}
+	public String getArac3plaka() {
+		return arac3plaka;
+	}
+	public void setArac3plaka(String arac3plaka) {
+		this.arac3plaka = arac3plaka;
+	}
+	public String getArac3isim() {
+		return arac3isim;
+	}
+	public void setArac3isim(String arac3isim) {
+		this.arac3isim = arac3isim;
+	}
+	public String getArac3marka() {
+		return arac3marka;
+	}
+	public void setArac3marka(String arac3marka) {
+		this.arac3marka = arac3marka;
+	}
+	public String getArac3adres() {
+		return arac3adres;
+	}
+	public void setArac3adres(String arac3adres) {
+		this.arac3adres = arac3adres;
+	}
+	public String getArac3durum() {
+		return arac3durum;
+	}
+	public void setArac3durum(String arac3durum) {
+		this.arac3durum = arac3durum;
+	}
+	public String getArac4plaka() {
+		return arac4plaka;
+	}
+	public void setArac4plaka(String arac4plaka) {
+		this.arac4plaka = arac4plaka;
+	}
+	public String getArac4isim() {
+		return arac4isim;
+	}
+	public void setArac4isim(String arac4isim) {
+		this.arac4isim = arac4isim;
+	}
+	public String getArac4marka() {
+		return arac4marka;
+	}
+	public void setArac4marka(String arac4marka) {
+		this.arac4marka = arac4marka;
+	}
+	public String getArac4adres() {
+		return arac4adres;
+	}
+	public void setArac4adres(String arac4adres) {
+		this.arac4adres = arac4adres;
+	}
+	public String getArac4durum() {
+		return arac4durum;
+	}
+	public void setArac4durum(String arac4durum) {
+		this.arac4durum = arac4durum;
+	}
+	public String getArac5plaka() {
+		return arac5plaka;
+	}
+	public void setArac5plaka(String arac5plaka) {
+		this.arac5plaka = arac5plaka;
+	}
+	public String getArac5isim() {
+		return arac5isim;
+	}
+	public void setArac5isim(String arac5isim) {
+		this.arac5isim = arac5isim;
+	}
+	public String getArac5marka() {
+		return arac5marka;
+	}
+	public void setArac5marka(String arac5marka) {
+		this.arac5marka = arac5marka;
+	}
+	public String getArac5adres() {
+		return arac5adres;
+	}
+	public void setArac5adres(String arac5adres) {
+		this.arac5adres = arac5adres;
+	}
+	public String getArac5durum() {
+		return arac5durum;
+	}
+	public void setArac5durum(String arac5durum) {
+		this.arac5durum = arac5durum;
+	}
+	
+
+}
